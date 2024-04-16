ef299c91b6fd3a598bf0e4eddba6cd34cf177790
David Veytsman <david.veytsman@gmail.com>
2024-04-16 15:14:31 -0400

    added sorting by toggle and prepending the list

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Simple Shopping list
 
+[Live Site](https://dveytsman.github.io/simple-shopping-list/)
+
 ## Purpose:
 
 I wanted to create a simple shopping list using the original front end methods, pre react and other new frameworks. This project uses vanilla js, html and css.
--- index.js
+++ index.js
@@ -1,33 +1,50 @@
 var button = document.getElementById("button");
 var input = document.getElementById("input");
-var list = document.getElementById("shopping-list");
-var listItems = document.querySelectorAll("li");
+var list = document.getElementById("list");
 
 function addListener(item) {
   item.addEventListener("click", function () {
     item.classList.toggle("done");
+    sortList();
   });
 }
-const isInputValid = () => input.value.length > 0;
+
+function sortList() {
+  const listItems = document.querySelectorAll("li");
+  const sortedItems = Array.from(listItems).sort(sortByStrikeThrough);
+  list.innerHTML = "";
+  sortedItems.forEach((item) => list.appendChild(item));
+}
+
+const isInputValid = () => input.value.trim() !== "";
+
 const addListItem = () => {
-  var li = document.createElement("li");
-  li.textContent = input.value;
-  addListener(li);
-  list.appendChild(li);
-  input.value = "";
+  if (isInputValid()) {
+    var li = document.createElement("li");
+    li.textContent = input.value.trim();
+    addListener(li);
+    list.prepend(li);
+    input.value = "";
+  }
 };
 
-button.addEventListener("click", function () {
-  if (isInputValid()) {
-    addListItem();
-  }
-});
+button.addEventListener("click", addListItem);
+
 input.addEventListener("keydown", function (event) {
-  if (event.key === "Enter" && isInputValid()) {
+  if (event.key === "Enter") {
     addListItem();
   }
 });
 
-listItems.forEach((item) => {
-  addListener(item);
+document.addEventListener("DOMContentLoaded", function () {
+  const listItems = document.querySelectorAll("li");
+  listItems.forEach((item) => {
+    addListener(item);
+  });
 });
+
+function sortByStrikeThrough(a, b) {
+  const strikeThroughA = a.classList.contains("done");
+  const strikeThroughB = b.classList.contains("done");
+  return strikeThroughA === strikeThroughB ? 0 : strikeThroughA ? 1 : -1;
+}
